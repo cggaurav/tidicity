bc8fd9e25e8b1030db78e2b9236ae439bf5d0dbf
Gaurav Chandrashekar <cggaurav@gmail.com>
2012-09-20 01:16:31 +0800

    Hello Tidicity

---
+++ src/com/android/tidicity/MainActivity.java
@@ -0,0 +1,23 @@
+package com.android.tidicity;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+import android.app.Activity;
+import android.view.Menu;
+
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
