ef9b47216ea9a1fd5fba2706c08f1d8a7fff0485
Mohit Kanwal <mohit.kanwal@gmail.com>
2012-10-17 03:52:26 +0800

    Modular Backbone migration

---
+++ assets/js/router.js
@@ -0,0 +1,44 @@
+// Filename: router.js
+define([
+  'jquery',
+  'underscore',
+  'backbone'
+], function($, _, Backbone ){
+  var AppRouter = Backbone.Router.extend({
+    routes: {
+      // Default
+      '*actions': 'defaultAction'
+    }
+  });
+  var initialize = function(){
+    var app_router = new AppRouter;
+    // app_router.on('route:showProjects', function(){
+    //   require(['views/projects/list'], function(ProjectListView) {
+    //     // Call render on the module we loaded in via the dependency array
+    //     // 'views/projects/list'
+    //     var projectListView = new ProjectListView();
+    //     projectListView.render();
+    //   })
+    // });
+    // app_router.on('route:showUsers', function () {
+    //   require(['views/users/list'], function(UserListView) {
+    //     // As above, call render on our loaded module
+    //     // 'views/users/list'
+    //     var userListView = new UserListView();
+    //     userListView.render();
+    //   });
+    // });
+    app_router.on('route:defaultAction', function (actions) {
+      console.log('routing started');
+      // require(['views/home/main'], function(MainHomeView) {
+      // // We have no matching route, lets display the home page 
+      //   var mainHomeView = new MainHomeView();
+      //   mainHomeView.render();
+      // });
+    });
+    Backbone.history.start();
+  };
+  return { 
+    initialize: initialize
+  };
+});
---
+++ server.js
@@ -0,0 +1,9 @@
+/* install connect first */
+// npm install connect
+
+var connect = require('connect');
+connect.createServer(
+    connect.static(__dirname)
+).listen(8080);
+
+/* run as localhost:8080/www/index.html */
