583346e9a4c309a4259401306cc220a8b62e8016
Mohit Kanwal <mohit.kanwal@gmail.com>
2012-10-29 04:08:32 +0800

    Working Geolocation, just need to bind before making a call

--- assets/js/libs/tidicity_utils/tidicity_utils.js
+++ assets/js/libs/tidicity_utils/tidicity_utils.js
@@ -18,12 +18,21 @@ define([], function() {
      * Utils.someFunction(...)
      *
      */
-    // onSuccess Callback
-    //   This method accepts a `Position` object, which contains
-    //   the current GPS coordinates
-    //
+    
+    /**
+     * [onLocationSuccess onsucess callback]
+     * @param  {[type]} position [This method accepts a `Position` object]
+     * @return {[type]}          [the current GPS coordinates]
+     */
     onLocationSuccess: function(position) {
-      alert('Latitude: ' + position.coords.latitude + '\n' + 'Longitude: ' + position.coords.longitude + '\n' + 'Altitude: ' + position.coords.altitude + '\n' + 'Accuracy: ' + position.coords.accuracy + '\n' + 'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '\n' + 'Heading: ' + position.coords.heading + '\n' + 'Speed: ' + position.coords.speed + '\n' + 'Timestamp: ' + new Date(position.timestamp) + '\n');
+      alert('Latitude: ' + position.coords.latitude + '\n' + 
+        'Longitude: ' + position.coords.longitude + '\n' + 
+        'Altitude: ' + position.coords.altitude + '\n' + 
+        'Accuracy: ' + position.coords.accuracy + '\n' + 
+        'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '\n' + 
+        'Heading: ' + position.coords.heading + '\n' + 
+        'Speed: ' + position.coords.speed + '\n' + 
+        'Timestamp: ' + new Date(position.timestamp) + '\n');
     },
 
     // onError Callback receives a PositionError object
@@ -33,10 +42,10 @@ define([], function() {
     },
 
     /**
-     * [getCurrentPosition Geolocation provides location information for the device, such as latitude and longitude. Common sources of location information include Global Positioning System (GPS) and location inferred from network signals such as IP address, RFID, WiFi and Bluetooth MAC addresses, and GSM/CDMA cell IDs. No guarantee is given that the API returns the device's actual location.
-     * 
-     * This API is based on the W3C Geo location API Specification. Some devices already provide an implementation of this spec. For those devices, the built-in support is used instead of replacing it with PhoneGap's implementation. For devices that don't have geolocation support, PhoneGap's implementation should be compatible with the W3C specification]
-     * 
+     * [getCurrentPosition Geolocation provides location information for the
+     * device, such as latitude and longitude]
+     *
+     *
      * @param  {[type]} onSuccess [Success Callback that recvs a Position Obj]
      * @param  {[type]} onError   [error callback]
      * @return {[type]}           [description]
